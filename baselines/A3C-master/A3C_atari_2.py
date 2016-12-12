e9284b2f7a049844e4274d05c8bd70e90142ddef
Kaixiang Lin <linkaixi@msu.edu>
2016-12-12 11:18:00 -0500

    modify A3C-master

--- baselines/A3C-master/A3C_atari_2.py
+++ baselines/A3C-master/A3C_atari_2.py
@@ -10,10 +10,10 @@ import gym
 import scipy.signal
 
 import cv2
-from tensorflow.RNNs.rnn_cell import BasicLSTMCell
+# from tensorflow.RNNs.rnn_cell import BasicLSTMCell
 from common import *
 
-tf.app.flags.DEFINE_string("game", "Breakout-v0", "gym environment name")
+tf.app.flags.DEFINE_string("game", "Pong-v0", "gym environment name")
 tf.app.flags.DEFINE_string("train_dir", "./models/experiment0/", "gym environment name")
 tf.app.flags.DEFINE_integer("gpu", 0, "gpu id")
 tf.app.flags.DEFINE_bool("use_lstm", False, "use LSTM layer")
@@ -32,7 +32,7 @@ tf.app.flags.DEFINE_float("eps", 1e-8, "param of smooth")
 tf.app.flags.DEFINE_float("entropy_beta", 1e-2, "param of policy entropy weight")
 tf.app.flags.DEFINE_float("gamma", 0.95, "discounted ratio")
 
-tf.app.flags.DEFINE_float("train_step", 0, "train step. unchanged")
+tf.app.flags.DEFINE_float("train_step", 10, "train step. unchanged")
 
 flags = tf.app.flags.FLAGS
 
@@ -140,138 +140,6 @@ class A3CNet(object):
                 self.pi_w1, self.pi_b1, self.pi_w2, self.pi_b2,
                 self.v_w1, self.v_b1, self.v_w2, self.v_b2]
 
-class A3CLSTMNet(object):
-    def __init__(self, state_shape, action_dim, scope):
-
-        class InnerLSTMCell(BasicLSTMCell):
-            def __init__(self, num_units, forget_bias=1.0, input_size=None):
-                BasicLSTMCell.__init__(self, num_units, forget_bias=forget_bias, input_size=input_size)
-                self.matrix, self.bias = None, None
-
-            def __call__(self, inputs, state, scope=None):
-                """
-                    Long short-term memory cell (LSTM).
-                    implement from BasicLSTMCell.__call__
-                """
-                with tf.variable_scope(scope or type(self).__name__):  # "BasicLSTMCell"
-                    # Parameters of gates are concatenated into one multiply for efficiency.
-                    c, h = tf.split(1, 2, state)
-                    concat = self.linear([inputs, h], 4 * self._num_units, True)
-
-                    # i = input_gate, j = new_input, f = forget_gate, o = output_gate
-                    i, j, f, o = tf.split(1, 4, concat)
-
-                    new_c = c * tf.sigmoid(f + self._forget_bias) + tf.sigmoid(i) * tf.tanh(j)
-                    new_h = tf.tanh(new_c) * tf.sigmoid(o)
-
-                    return new_h, tf.concat(1, [new_c, new_h])
-
-            def linear(self, args, output_size, bias, bias_start=0.0, scope=None):
-                """
-                    Linear map: sum_i(args[i] * W[i]), where W[i] is a variable.
-                    implement from function of tensorflow.python.ops.rnn_cell.linear()
-                """
-                if args is None or (isinstance(args, (list, tuple)) and not args):
-                    raise ValueError("`args` must be specified")
-                if not isinstance(args, (list, tuple)):
-                    args = [args]
-
-                    # Calculate the total size of arguments on dimension 1.
-                total_arg_size = 0
-                shapes = [a.get_shape().as_list() for a in args]
-                for shape in shapes:
-                    if len(shape) != 2:
-                        raise ValueError("Linear is expecting 2D arguments: %s" % str(shapes))
-                    if not shape[1]:
-                        raise ValueError("Linear expects shape[1] of arguments: %s" % str(shapes))
-                    else:
-                        total_arg_size += shape[1]
-
-                # Now the computation.
-                with tf.variable_scope(scope or "Linear"):
-                    matrix = tf.get_variable("Matrix", [total_arg_size, output_size])
-                    if len(args) == 1:
-                        res = tf.matmul(args[0], matrix)
-                    else:
-                        res = tf.matmul(tf.concat(1, args), matrix)
-                    if not bias:
-                        return res
-                    bias_term = tf.get_variable(
-                        "Bias", [output_size],
-                        initializer=tf.constant_initializer(bias_start))
-                    self.matrix = matrix
-                    self.bias = bias_term
-                return res + bias_term
-
-        with tf.device("/gpu:%d" % flags.gpu):
-            # placeholder
-            self.state = tf.placeholder(tf.float32, shape=[None] + list(state_shape), name="state")  # (None, 84, 84, 4)
-            self.action = tf.placeholder(tf.float32, shape=[None, action_dim], name="action")  # (None, actions)
-            self.target_q = tf.placeholder(tf.float32, shape=[None])
-            # shared parts
-            with tf.variable_scope("%s_shared" % scope):
-                conv1, self.w1, self.b1 = conv2d(self.state, (8, 8, state_shape[-1], 16), "conv_1", stride=4,
-                                                 padding="VALID", with_param=True)  # (None, 20, 20, 16)
-                conv2, self.w2, self.b2 = conv2d(conv1, (4, 4, 16, 32), "conv_2", stride=2,
-                                                 padding="VALID", with_param=True)  # (None, 9, 9, 32)
-                flat1 = tf.reshape(conv2, (-1, 9 * 9 * 32), name="flat1")
-                fc_1, self.w3, self.b3 = full_connect(flat1, (9 * 9 * 32, 256), "fc1", with_param=True)
-            # rnn parts
-            with tf.variable_scope("%s_rnn" % scope):
-                h_flat1 = tf.reshape(fc_1, (1, -1, 256))
-                self.lstm = InnerLSTMCell(256)
-                self.initial_lstm_state = tf.placeholder(tf.float32, shape=[1, self.lstm.state_size], name="init_lstm_state")
-                self.sequence_length = tf.placeholder(tf.int32, [1], name="seq_length")
-                lstm_outputs, self.lstm_state = tf.nn.dynamic_rnn(self.lstm, h_flat1,
-                                                                  initial_state=self.initial_lstm_state,
-                                                                  sequence_length=self.sequence_length,
-                                                                  time_major=False,
-                                                                  scope=scope)
-                lstm_outputs = tf.reshape(lstm_outputs, [-1, 256])
-            # policy parts
-            with tf.variable_scope("%s_policy" % scope):
-                pi_fc_1, self.pi_w1, self.pi_b1 = full_connect(lstm_outputs, (256, 256), "pi_fc1", with_param=True)
-                pi_fc_2, self.pi_w2, self.pi_b2 = full_connect(pi_fc_1, (256, action_dim), "pi_fc2", activate=None,
-                                                               with_param=True)
-                self.policy_out = tf.nn.softmax(pi_fc_2, name="pi_out")
-            # value parts
-            with tf.variable_scope("%s_value" % scope):
-                v_fc_1, self.v_w1, self.v_b1 = full_connect(lstm_outputs, (256, 256), "v_fc1", with_param=True)
-                v_fc_2, self.v_w2, self.v_b2 = full_connect(v_fc_1, (256, 1), "v_fc2", activate=None, with_param=True)
-                self.value_out = tf.reshape(v_fc_2, [-1], name="v_out")
-            # loss values
-            with tf.op_scope([self.policy_out, self.value_out], "%s_loss" % scope):
-                self.entropy = - tf.reduce_mean(self.policy_out * tf.log(self.policy_out + flags.eps))
-                time_diff = self.target_q - self.value_out
-                policy_prob = tf.log(tf.reduce_sum(tf.mul(self.policy_out, self.action), reduction_indices=1))
-                self.policy_loss = - tf.reduce_sum(policy_prob * time_diff)
-                self.value_loss = tf.reduce_sum(tf.square(time_diff))
-                self.total_loss = self.policy_loss + self.value_loss * 0.5 + self.entropy * flags.entropy_beta
-        # lstm state
-        self.lstm_state_out = np.zeros((1, self.lstm.state_size), dtype=np.float32)
-
-    def reset_lstm_state(self):
-        self.lstm_state_out = np.zeros((1, self.lstm.state_size), dtype=np.float32)
-
-    def get_policy(self, sess, state):
-        policy_out, self.lstm_state_out = sess.run([self.policy_out, self.lstm_state],
-                                                   feed_dict={self.state: [state],
-                                                              self.initial_lstm_state: self.lstm_state_out,
-                                                              self.sequence_length: [1]})
-        return policy_out[0]
-
-    def get_value(self, sess, state):
-        value_out, _ = sess.run([self.value_out, self.lstm_state], feed_dict={self.state: [state],
-                                                                              self.initial_lstm_state: self.lstm_state_out,
-                                                                              self.sequence_length: [1]})
-        return value_out[0]
-
-    def get_vars(self):
-        return [self.w1, self.b1, self.w2, self.b2, self.w3, self.b3,
-                self.lstm.matrix, self.lstm.bias,
-                self.pi_w1, self.pi_b1, self.pi_w2, self.pi_b2,
-                self.v_w1, self.v_b1, self.v_w2, self.v_b2]
-
 
 class A3CSingleThread(threading.Thread):
     def __init__(self, thread_id, master):
@@ -280,10 +148,8 @@ class A3CSingleThread(threading.Thread):
         self.env = AtariEnv(gym.make(flags.game))
         self.master = master
         # local network
-        if flags.use_lstm:
-            self.local_net = A3CLSTMNet(self.env.state_shape, self.env.action_dim, scope="local_net_%d" % thread_id)
-        else:
-            self.local_net = A3CNet(self.env.state_shape, self.env.action_dim, scope="local_net_%d" % thread_id)
+        self.local_net = A3CNet(self.env.state_shape, self.env.action_dim, scope="local_net_%d" % thread_id)
+
         # sync network
         self.sync = self.sync_network(master.shared_net)
         # accumulate gradients
@@ -396,8 +262,8 @@ class A3CSingleThread(threading.Thread):
             if rollout_path["done"][-1]:
                 rollout_path["rewards"][-1] = 0
                 self.env.reset_env()
-                if flags.use_lstm:
-                    self.local_net.reset_lstm_state()
+                # if flags.use_lstm:
+                #     self.local_net.reset_lstm_state()
             else:
                 rollout_path["rewards"][-1] = self.local_net.get_value(sess, rollout_path["state"][-1])
             rollout_path["returns"] = self.discount(rollout_path["rewards"])
@@ -406,14 +272,14 @@ class A3CSingleThread(threading.Thread):
             fetches = [self.do_accum_grads_ops, self.master.global_step]
             if loop % 10 == 0:
                 fetches.append(self.summary_op)
-            if flags.use_lstm:
-                res = sess.run(fetches, feed_dict={lc_net.state: rollout_path["state"],
-                                                   lc_net.action: rollout_path["action"],
-                                                   lc_net.target_q: rollout_path["returns"],
-                                                   lc_net.initial_lstm_state: lc_net.lstm_state_out,
-                                                   lc_net.sequence_length: [1]})
-            else:
-                res = sess.run(fetches, feed_dict={lc_net.state: rollout_path["state"],
+            # if flags.use_lstm:
+            #     res = sess.run(fetches, feed_dict={lc_net.state: rollout_path["state"],
+            #                                        lc_net.action: rollout_path["action"],
+            #                                        lc_net.target_q: rollout_path["returns"],
+            #                                        lc_net.initial_lstm_state: lc_net.lstm_state_out,
+            #                                        lc_net.sequence_length: [1]})
+            # else:
+            res = sess.run(fetches, feed_dict={lc_net.state: rollout_path["state"],
                                                    lc_net.action: rollout_path["action"],
                                                    lc_net.target_q: rollout_path["returns"]})
             if loop % 10 == 0:
@@ -459,10 +325,8 @@ class A3CAtari(object):
         self.env = AtariEnv(gym.make(flags.game))
         self.graph = tf.get_default_graph()
         # shared network
-        if flags.use_lstm:
-            self.shared_net = A3CLSTMNet(self.env.state_shape, self.env.action_dim, scope="global_net")
-        else:
-            self.shared_net = A3CNet(self.env.state_shape, self.env.action_dim, scope="global_net")
+
+        self.shared_net = A3CNet(self.env.state_shape, self.env.action_dim, scope="global_net")
         # shared optimizer
         self.shared_opt, self.global_step, self.summary_writer = self.shared_optimizer()
         # local training threads
